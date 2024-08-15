ab7f5522409ea3861a9c647e8ffa78ad96c2dc3d
Pratik Patel <pratik139patel@gmail.com>
2024-08-14 22:04:37 -0500

    update website

--- js/custom.js
+++ js/custom.js
@@ -1,46 +1,86 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-
-getYear();
-
-
-// client section owl carousel
-$(".client_owl-carousel").owlCarousel({
-    loop: true,
-    margin: 20,
-    dots: false,
-    nav: true,
-    navText: [],
-    autoplay: true,
-    autoplayHoverPause: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 2
-        }
+var hero = $(".hero_area");
+var menu = $(".custom_menu-container");
+$(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > hero.height()) {
+        menu.addClass("menu_fixed-position");
+        $(".custom_menu-container + section").addClass("mt-5");
+    } else {
+        menu.removeClass("menu_fixed-position");
+        $(".custom_menu-container + section").removeClass("mt-5");
     }
 });
 
 
-
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+var myCircle = Circles.create({
+    id: "circles-1",
+    radius: 65,
+    value: 85,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+var myCircle = Circles.create({
+    id: "circles-2",
+    radius: 65,
+    value: 55,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+var myCircle = Circles.create({
+    id: "circles-3",
+    radius: 65,
+    value: 65,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
+var myCircle = Circles.create({
+    id: "circles-4",
+    radius: 65,
+    value: 85,
+    maxValue: 100,
+    width: 5,
+    text: function (value) {
+        return value + "%";
+    },
+    colors: ["transparent", "#ffffff"],
+    duration: 400,
+    wrpClass: "circles-wrp",
+    textClass: "progress_text",
+    valueStrokeClass: "circles-valueStroke",
+    maxValueStrokeClass: "circles-maxValueStroke",
+    styleWrapper: true,
+    styleText: true
+});
